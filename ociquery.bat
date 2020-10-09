035adf475bf54a387e572c6f67ea1b43b6f54f08
Anderson de Almeida <70177495+andersonalmeida1@users.noreply.github.com>
2020-10-09 16:09:58 -0300

    Create ociquery.bat

---
+++ ociquery.bat
@@ -0,0 +1,2 @@
+echo %1
+oci --config-file ../config search resource structured-search  --query-text "query %1 resources "  --query "data.items[].[\"resource-type\",\"display-name\",\"lifecycle-state\"]" --output %2
